521161535055f09bbbf31195fe39b15d4e10071f
santiagodls <itnas06@gmail.com>
2016-11-10 13:49:03 +0100

    Estilos base para index

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -3,3 +3,7 @@ $(function(){
 		$(".menu-launcher, .menu-fs, header").toggleClass("active");
 	})
 });
+$(window).scroll(function(){
+	var scrollPos = $(this).scrollTop();
+	$("#portada").css("background-position", "0px " + -scrollPos/4 +"px")
+});
