cb8528b68f738f0cba278380c8c028d871d5e8c8
santiagodls <itnas06@gmail.com>
2016-11-16 22:59:45 +0100

    Script para rotar la última palabra del eslogan de la portada

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,8 +1,27 @@
+//LAUNCHERS
 $(function(){
 	$(".menu-launcher").click(function(){
 		$(".menu-launcher, .menu-fs, header").toggleClass("active");
 	})
 });
+
+//ANIMATION
+function textRotator() {
+	var current = $("#dinamic-end b.visible"),
+			first = $("#dinamic-end b").first(),
+			params = "visible hidden";
+	if (current.next().length == 0) {
+		current.toggleClass(params);
+		first.toggleClass(params);
+	}
+	else {
+		current.toggleClass(params).next().toggleClass(params);
+	}
+	setTimeout(textRotator, 3500);
+}
+textRotator();
+
+//PARALLAX
 $(window).scroll(function(){
 	var scrollPos = $(this).scrollTop();
 	$("#cover").css("background-position", "0px " + -scrollPos/4 +"px")
