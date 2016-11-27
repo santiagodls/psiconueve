8a3f4ae8b27178acc0eb22714fe3dd1f54a2e02d
santiagodls <itnas06@gmail.com>
2016-11-27 21:33:53 +0100

    Formspree y ajax para el formulario

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -21,6 +21,7 @@ $(function(){
 
 //ACTIONS
 $(function(){
+	//menu
 	$(".menu-launcher").click(function(){
 		$(".menu-launcher, #menu-fs, #quotes, header").toggleClass("active");
 		$("body").toggleClass("no-scroll");
@@ -35,6 +36,7 @@ $(function(){
 		var link = $(this).children().attr("href");
 		location.href = link;
 	})
+	//go top
 	var goPos = parseInt($(".go-top").css("top"));
 	$(".go-top").click(function(){
 		$("html, body").animate({scrollTop: 0}, 750, "easeInOutExpo");
@@ -80,6 +82,71 @@ $(function(){
 		$(".jarallax").addClass("ms").jarallax("destroy");
 	}
 });
+
+//FORMS
+$(function(){
+	//pass select value
+	selectedValue();
+	$("#formulario-consultar select").change(function(){
+		selectedValue();
+	});
+
+	//handle submit
+	$("#contactar, #consultar").click(function(){
+		var value = $(this).attr("value").toLowerCase(),
+		form = $("#formulario-"+value),
+		message = form.serialize(),
+		requireds = form.find("input, textarea, select").filter("[required]"),
+		status;
+		//Act normal if empty required
+		requireds.each(function(i, e){
+			if($(e).val() == "") {
+				status = true;
+				return;
+			}
+		});
+		if (status == true) {
+			return;
+		}
+		//else continue to ajax
+		$.ajax({
+			url: "https://formspree.io/itnas06@gmail.com",
+			method: "POST",
+			data: message,
+			dataType: "json",
+			beforeSend: function() {
+				form.append('<div id="form-popups"><div class="cssload-thecube"><div class="cssload-cube cssload-c1"></div><div class="cssload-cube cssload-c2"></div><div class="cssload-cube cssload-c4"></div><div class="cssload-cube cssload-c3"></div></div></div>');
+				$("body").addClass("no-scroll-modal");
+			},
+			success: function(data) {
+				var wrapper = $("#form-popups");
+				wrapper.find('.cssload-thecube').remove();
+				wrapper.append('<div class="alert success"><b>Mensaje enviado correctamente!</b><br> Intentaremos responderle en la brevedad</div>');
+				removeWrapper(form);
+				form[0].reset();
+			},
+			error: function(err) {
+				var wrapper = $("#form-popups");
+				wrapper.find('.cssload-thecube').remove();
+				wrapper.append('<div class="alert error">Hubo un problema, revisa el formulario...</div>');
+				console.log(err)
+				removeWrapper(form);
+			}
+		});
+		return false;
+	});
+});
+function removeWrapper(form) {
+	$("#form-popups").delay(2000).animate({opacity: 0}, 500, function(){
+		$(this).remove();
+	});
+	$("body").removeClass("no-scroll-modal");
+}
+function selectedValue() {
+	var select = $("#formulario-consultar select"),
+			selectTarget = $("#select-target");
+	selectTarget.attr("name", select.find(":selected").attr("value"));
+}
 
 //LIVERELOAD KEEP SCROLL
 var timer;
